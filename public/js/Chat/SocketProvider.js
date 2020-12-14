1384fb51d6c0a41b3069b138e34db61256397cd9
ZOE <zoetw88@gmail.com>
2020-12-15 07:46:53 +0800

    update chatroom frontend

--- public/js/Chat/SocketProvider.js
+++ public/js/Chat/SocketProvider.js
@@ -1,5 +1,3 @@
-
-
 let receiver = "";
 let sender = "";
 
@@ -18,41 +16,45 @@ if (localStorage.getItem("token")) {
                     id: sender
                 }
             })
-           
+
             $('#profile').find('p').text(sender)
-            sender='zoe'
-            
-            function organize_talk(response,username){
-                for(let i =0;i<response.data.length;i++){
-                    if(response.data[i].sender==username){
-                        $('<li class="sent"><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><p>' +response.data[i].message + '</p></li>').appendTo($('.messages ul'));
-                    }else{
+            sender = 'zoe'
+
+            function organize_talk(response, username) {
+                for (let i = 0; i < response.data.length; i++) {
+                    if (response.data[i].sender == username) {
+                        $('<li class="sent"><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><p>' + response.data[i].message + '</p></li>').appendTo($('.messages ul'));
+                    } else {
                         $('<li class="replies"><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><p>' + response.data[i].message + '</p></li>').appendTo($('.messages ul'));
                     }
                 }
-                    
+
             }
-            function organize_talker(response,username){
-                $('<li class="contact active"><div class="wrap"><span class="contact-status online"></span><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><div class="meta"><p class="name">' +response.data[0].receiver + '</p><p class="preview">' +response.data[0].message + '</p></div></div></li>').appendTo($('#contacts ul'));
-                for(let i =1;i<response.data.length;i++){
-                    
-                        
-                      
-                    if(response.data[i].sender==username){
+
+            function organize_talker(response, username) {
+                $('<li class="contact active"><div class="wrap"><span class="contact-status online"></span><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><div class="meta"><p class="name">' + response.data[0].receiver + '</p><p class="preview">' + response.data[0].message + '</p></div></div></li>').appendTo($('#contacts ul'));
+                for (let i = 1; i < response.data.length; i++) {
+
+
+
+                    if (response.data[i].sender == username) {
                         console.log('ok')
-                        $('<li class="contact"><div class="wrap"><span class="contact-status online"></span><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><div class="meta"><p class="name">' +response.data[i].receiver + '</p><p class="preview">' +response.data[i].message + '</p></div></div></li>').appendTo($('#contacts ul'));
-                    }else{
+                        $('<li class="contact"><div class="wrap"><span class="contact-status online"></span><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><div class="meta"><p class="name">' + response.data[i].receiver + '</p><p class="preview">' + response.data[i].message + '</p></div></div></li>').appendTo($('#contacts ul'));
+                    } else {
                         console.log('yes')
-                        $('<li class="contact"><div class="wrap"><span class="contact-status online"></span><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><div class="meta"><p class="name">' +response.data[i].sender + '</p><p class="preview">' +response.data[i].message + '</p></div></div></li>').appendTo($('#contacts ul'));
+                        $('<li class="contact"><div class="wrap"><span class="contact-status online"></span><img src="https://zoesandbox.s3-ap-southeast-1.amazonaws.com/img/1604398861214-orange.jpg" alt="" /><div class="meta"><p class="name">' + response.data[i].sender + '</p><p class="preview">' + response.data[i].message + '</p></div></div></li>').appendTo($('#contacts ul'));
                     }
                 }
-                    
+
             }
+
             function getSideMessage(username) {
-                axios.post(`http://localhost:5000/get_side_messages`,{username:username})
+                axios.post(`http://localhost:5000/get_side_messages`, {
+                        username: username
+                    })
                     .then((response) => {
                         console.log(response)
-                        organize_talker(response,username)
+                        organize_talker(response, username)
                     })
                     .catch((error) => {
                         console.log(error)
@@ -70,11 +72,14 @@ if (localStorage.getItem("token")) {
 
 
             }
+
             function getMainMessage(username) {
-                axios.post(`http://localhost:5000/get_main_messages`,{username:username})
+                axios.post(`http://localhost:5000/get_main_messages`, {
+                        username: username
+                    })
                     .then((response) => {
                         console.log(response)
-                        organize_talk(response,username)
+                        organize_talk(response, username)
                     })
                     .catch((error) => {
                         console.log(error)
@@ -93,34 +98,49 @@ if (localStorage.getItem("token")) {
 
             }
 
-            
+
             getMainMessage(sender)
             getSideMessage(sender)
-            function onUserSelected(chosenName,username) {
-                axios.post(`http://localhost:5000/get_select_messages`,{username:username,chosenName:chosenName})
-                .then((response) => {
-                    console.log(response)
-                    organize_talk(response,username)
-                })
-                .catch((error) => {
-                    console.log(error)
-                    if (!error.response) {
-                        // network error
-                    } else {
-                        // http status code
-                        const code = error.response.status
-                        // response data
-                        const response = error.response.data
-                        console.loge(code)
+
+            function onUserSelected(chosenName, username) {
+                axios.post(`http://localhost:5000/get_select_messages`, {
+                        username: username,
+                        chosenName: chosenName
+                    })
+                    .then((response) => {
                         console.log(response)
-                    }
-                })
-        
+                        organize_talk(response, username)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                        if (!error.response) {
+                            // network error
+                        } else {
+                            // http status code
+                            const code = error.response.status
+                            // response data
+                            const response = error.response.data
+                            console.loge(code)
+                            console.log(response)
+                        }
+                    })
+
             }
-          
+
+
+            $(document).on('click', '.contact', function () {
+                $(".contact.active").removeClass("active");
+                $(this).addClass('active');
+                $( "li" ).remove( ".sent" );
+                $( "li" ).remove( ".replies" );
+                let chosenName = $(this).find('.name').text();
+
+                onUserSelected(chosenName,sender)
+            })
+
 
             function newMessage() {
-               
+
                 receiver = $('.active').find('.name').text();
                 console.log(receiver)
                 message = $(".message-input input").val();
@@ -139,7 +159,7 @@ if (localStorage.getItem("token")) {
                     receiver: receiver,
                     message: message
                 });
-            
+
             }
             $('.submit').click(function () {
                 newMessage();
