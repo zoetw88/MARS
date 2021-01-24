0dd80660279b504e9c4f309d86d354502682dbd8
ZOE <zoetw88@gmail.com>
2021-01-24 15:00:07 +0800

    repair chart bug

--- public/assets/js/chart/chart2.js
+++ public/assets/js/chart/chart2.js
@@ -14,27 +14,27 @@ Plotly.d3.json('../../json/salary.json', function(figure) {
       data.push(trace);
     }
     let y_legend;
-    if (data.length<6&&data.length>3) {
-      y_legend=-0.8;
-      chart_height=545;
-    } else if (data.length>6&&data.length<11) {
-      y_legend=-1.3;
-      chart_height=670; } 
-      else if (data.length>10&&data.length<13) {
-      y_legend=-1.3;
-      chart_height=700; } 
-      else if (data.length>13&& data.length<15) {
-        y_legend=-1.7;
-        chart_height=900;
-    }  else if (data.length=15) {
-      y_legend=-1.7;
-      chart_height=780;
-  } else if (data.length>=16) {
-      y_legend=-1.9;
-      chart_height=800;
-    }else{
-      y_legend=-0.8;
-      chart_height=530;
+    if (data.length ==3) {
+      y_legend = -0.8;
+      chart_height = 520;
+    } else if (data.length > 6 && data.length < 11) {
+      y_legend = -1.3;
+      chart_height = 665;
+    } else if (data.length > 10 && data.length < 13) {
+      y_legend = -1.3;
+      chart_height = 700;
+    } else if (data.length > 13 && data.length < 15) {
+      y_legend = -1.7;
+      chart_height = 900;
+    } else if (data.length == 15) {
+      y_legend = -1.7;
+      chart_height = 780;
+    } else if (data.length >= 16) {
+      y_legend = -1.9;
+      chart_height = 800;
+    } else if (data.length < 7)  {
+      y_legend = -0.9;
+      chart_height = 550;
     }
     const layout = {
       width: 520,
